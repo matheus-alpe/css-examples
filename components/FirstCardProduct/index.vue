3a68bf81aff7c2e57bb9d6997c4f6ee8d5b6f396
Matheus Alves Pereira <matttalves@gmail.com>
2023-02-22 13:09:37 -0300

    feat: 1 card

---
+++ components/FirstCardProduct/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card">
+    <div class="card__img">
+      <img
+        src="@/assets/images/headphone-philips.png"
+        alt="Headphone Philips"
+      />
+    </div>
+    <div class="card__content">
+      <h3 class="card__content-title">Wireless Headphone</h3>
+
+      <h2 class="card__content-price">$40.<small>99</small></h2>
+
+      <a
+        href="#"
+        class="card__content-buy"
+      >
+        Buy Now
+      </a>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import './FirstCardProduct.scss';
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <script lang="ts" setup></script>
 
 <template>
-  <h1>Home</h1>
+  <nav>
+    <ul>
+      <li>
+        <NuxtLink to="1-card-product">1 - Card Product</NuxtLink>
+      </li>
+    </ul>
+  </nav>
 </template>
